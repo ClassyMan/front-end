1e9d44d2f28bfe4f8d534d7be2146ce76c4c283f
Aidan <yardy_14@hotmail.com>
2018-06-25 20:21:03 -0400

    working on timestamp ordering. doesnt work yet

--- src/Discussions.js
+++ src/Discussions.js
@@ -5,24 +5,80 @@ export default class Discussions extends Component {
   constructor(props) {
         super(props);
     this.state = {
-      discussions: []
+      discussions: [],
+      title: '',
+      summary: '',
+      addingNewDiscussion: false
     }
+
+    this.handleAddNewDiscussion = this.handleAddNewDiscussion.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleSummaryChange = this.handleSummaryChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   render() {
-    return <div>
-             <button>Add new discussion</button>
-             <p>Discussions</p>
-             {
-               this.state.discussions.map(discussion => {
-                                return <div key={discussion.id}>
-                                         <dt>{discussion.title}</dt>
-                                         <button value={discussion.id}>Open</button>
-                                         <hr></hr>
-                                       </div>
-               })
-             }
-           </div>
+    if (this.state.addingNewDiscussion) {
+      return <div>
+               <form onSubmit={this.handleSubmit}>
+                 <label>
+                   Title:
+                   <input type="text" value={this.state.title} onChange={this.handleTitleChange} />
+                 </label>
+                 <br/>
+                 <label>
+                   Summary:
+                   <input type="text" value={this.state.summary} onChange={this.handleSummaryChange} />
+                 </label>
+                 <br/>
+               <input type="submit" value="Submit" />
+             </form>
+             </div>
+    } else {
+      return <div>
+               <button onClick={this.handleAddNewDiscussion}>Add new discussion</button>
+               <p>Discussions</p>
+               {
+                 this.state.discussions.map(discussion => {
+                                  return <div key={discussion.id}>
+                                           <h1>{discussion.title}</h1>
+                                           <p>{discussion.summary}</p>
+                                           <button value={discussion.id}>Open</button>
+                                           <hr></hr>
+                                         </div>
+                 })
+               }
+             </div>
+    }
+  }
+
+  handleAddNewDiscussion() {
+    this.setState({addingNewDiscussion: true});
+  }
+
+  handleTitleChange(event) {
+    this.setState({title: event.target.value});
+  }
+
+  handleSummaryChange(event) {
+    console.log('handling state change: ' + event.target.value);
+    this.setState({summary: event.target.value})
+  }
+
+  handleSubmit(event) {
+    console.log('A discussion was submitted: ' + this.state.title);
+
+    fetch('http://localhost:8080/discussions/add', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: this.state.title,
+        summary: this.state.summary
+      })
+    });
   }
 
   componentDidMount() {
