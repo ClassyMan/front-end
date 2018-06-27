0af7c85536237f401c530ca4af820e55ad7d2209
Aidan <yardy_14@hotmail.com>
2018-06-27 19:22:10 -0400

    Adding a little bit of validation

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -19,8 +19,13 @@ class HomeScreen extends Component {
       this.handleLoginAttempt = this.handleLoginAttempt.bind(this);
       this.handleLogoutAttempt = this.handleLogoutAttempt.bind(this);
       this.handleRegistrationAttempt = this.handleRegistrationAttempt.bind(this);
+      this.stateIsValid = this.stateIsValid.bind(this);
   }
 
+  /*
+   * Render the login if the yser is not logged in
+   * Otherwise display the list of open discussions.
+   */
   render() {
     console.log('logged in status' + localStorage.getItem('loggedIn'));
     if (localStorage.getItem('loggedIn') === 'true') {
@@ -32,14 +37,23 @@ class HomeScreen extends Component {
       return <div>
                <input type="text" value={this.state.username} onChange={this.handleNameChange} />
                <input type="text" value={this.state.password} onChange={this.handlePasswordChange} />
-               <button onClick={this.handleLoginAttempt}>Login</button>
-               <button onClick={this.handleRegistrationAttempt}>Register</button>
+               <button disabled={!this.stateIsValid()} onClick={this.handleLoginAttempt}>Login</button>
+               <button disabled={!this.stateIsValid()} onClick={this.handleRegistrationAttempt}>Register</button>
              </div>
     }
 
   }
 
+  stateIsValid() {
+    if (this.state.username.length > 0 && this.state.password.length > 8) {
+      return true;
+    }
+  }
 
+  /*
+   * When a user attempts to log in, send an http request to verify that they
+   * are indeed a user and then cache the fact that they are logged in
+   */
   handleLoginAttempt(event) {
     console.log('Login attempt: ' + this.state.username);
 
@@ -69,6 +83,10 @@ class HomeScreen extends Component {
    });
   }
 
+  /*
+   * When a user logs out set both the state and the cached value to false for
+   * whether or not the user is logged in.
+   */
   handleLogoutAttempt(event) {
     console.log('logging out');
     this.setState({loggedIn: false});
@@ -77,6 +95,11 @@ class HomeScreen extends Component {
     console.log('localStorage login status set to ' + localStorage.getItem('loggedIn'));
   }
 
+  /*
+   * When a user attempts to register themselves we need to record them as a new
+   * user and send an http request to the back end so that they can be added to
+   * the users collection.
+   */
   handleRegistrationAttempt(event) {
     console.log('Registration has been attempted: ' + this.state.username);
 
@@ -98,13 +121,19 @@ class HomeScreen extends Component {
    });
   }
 
+  /*
+   * In the event of a name change, make sure that state is updated
+   */
   handleNameChange(event) {
     console.log('Handling username change: ' + event.target.value);
     this.setState({username: event.target.value});
   }
 
+  /*
+   * In the event of a password change, make sure that state is updated
+   * Do not log out the password as this will still be in plain text form.
+   */
   handlePasswordChange(event) {
-    console.log('Handling password change: ' + event.target.value);
     this.setState({password: event.target.value});
   }
 }
