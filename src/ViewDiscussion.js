eb59b808d432b65d55120f77703ed643dffebfbf
Aidan <yardy_14@hotmail.com>
2018-06-30 18:20:21 -0400

    fiddling with panels again

--- src/ViewDiscussion.js
+++ src/ViewDiscussion.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { Label, Button, FormGroup, FormControl } from 'react-bootstrap';
+import { Panel, Label, Button, FormGroup, FormControl } from 'react-bootstrap';
 import {headerSettings} from './HttpSettings.js';
 import Comment from './Comment.js';
 import { Link } from 'react-router';
-
 /*
  * Component for viewing and discussing a single discussion
  */
@@ -16,6 +15,8 @@ export default class ViewDiscussion extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      title: '',
+      summary: '',
       comments: [],
       username: '',
       content: '',
@@ -52,7 +53,12 @@ export default class ViewDiscussion extends Component {
     if (!this.state.addingNewComment) {
       return <div>
                <Button><Link to="/">Home</Link></Button>
-               <p>you are viewing the {this.props.params.id} discussion</p>
+               <Panel >
+                 <Panel.Heading>
+                   <Panel.Title>{this.state.title} : {this.props.params.id}</Panel.Title>
+                 </Panel.Heading>
+                   <Panel.Body>{this.state.summary}</Panel.Body>
+               </Panel>
                <Button onClick={this.handleAddNewComment.bind(this)}>Reply</Button>
                {
                  this.state.comments
@@ -123,7 +129,7 @@ export default class ViewDiscussion extends Component {
 
   loadPagedComments() {
     console.log('Loading comments for discussion: ' + this.props.params.id);
-    return fetch('http://localhost:8080/comments/fetchCommentsForDiscussion', {
+    fetch('http://localhost:8080/comments/fetchCommentsForDiscussion', {
         method: 'POST',
         headers: headerSettings,
         body: JSON.stringify({
@@ -142,5 +148,27 @@ export default class ViewDiscussion extends Component {
        var retreived = text.length ? JSON.parse(text) : {};
        this.setState({comments: retreived})
     });
+
+    fetch('http://localhost:8080/comments/fetchDiscussionById', {
+        method: 'POST',
+        headers: headerSettings,
+        body: JSON.stringify({
+          discussionId: this.props.params.id
+        })
+      }
+    )
+    .then((res) => {
+       console.log('response received');
+       var text = res.text();
+       console.log('result: ' + text);
+       return text;
+    })
+    .then((text) => {
+       console.log('parsing json: ' + text);
+       var retreived = text.length ? JSON.parse(text) : {};
+       this.setState({title: retreived.title,
+                      summary: retreived.summary})
+    });
+
   }
 }
