982a925163deca976557facf63d760d6bf447f19
Aidan <yardy_14@hotmail.com>
2018-07-03 21:19:15 -0400

    docz

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 
+/*
+ * This is the page people will see when their url for
+ * the app doesn't match anything which leads to an
+ * actual component or discussion etc. 
+ */
 export default class NotFound extends Component {
   render() {
     return <p>404, we got drunk and lost that page *BUUURP* sorry</p>
